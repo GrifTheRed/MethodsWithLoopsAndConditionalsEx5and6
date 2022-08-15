a3326517ae054981fd08ad7693cd9d00ca7b04ff
Derek Griffin <griffinderek6@gmail.com>
2022-08-15 07:44:52 -0600

    finished

---
+++ MethodsWithLoopsAndConditionalsEx5and6/Program.cs
@@ -0,0 +1,25 @@
+﻿//Write a method to check whether a
+//given number is positive or negative
+
+//int a = 32;
+//if (a >= 0)
+//{
+//    Console.WriteLine("positive");
+//}
+//else
+//{
+//    Console.WriteLine("negative");
+//}
+
+//Write a method to read the age of a candidate
+//and determine whether they can vote.
+
+var voterAge = 17;
+if(voterAge >= 18)
+{
+    Console.WriteLine("Yes you can vote!");
+}
+else
+{
+    Console.WriteLine("sorry, you still have some years to wait before you can vote");
+}
